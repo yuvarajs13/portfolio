edae7f40c8a0e9ac70c9e976531ac5b121b8ff1a
yuvarajs13 <yuvaraj93600@gmail.com>
2025-08-15 22:50:16 +0530

    header

---
+++ src/components/home/Data.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Data() {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">YUVARAJ
+            
+        </h1>
+    </div>
+  )
+}
+
+export default Data
---
+++ src/components/home/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./home.css"
+import Data from './Data'
+
+
+const Home =() => {
+  return (
+    <section className="home section" id="home">
+        <div className="home__container container grid">
+          <div className="home__content grid">
+            <div className="home__img"></div>
+            <Data />
+          </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
