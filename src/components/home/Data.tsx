b1bf98ad4a1ccafb604293785dcf52fc1dbace81
yuvarajs13 <yuvaraj93600@gmail.com>
2025-08-23 23:45:48 +0530

    almost there

--- src/components/home/Data.tsx
+++ src/components/home/Data.tsx
@@ -3,13 +3,28 @@ import React from 'react'
 function Data() {
   return (
     <div className="home__data">
-        <h1 className="home__title">YUVARAJ
-            
-        </h1>
-        <h3 className="home__subtitle">App Developer </h3>
-        <p className="home__description">Building applications with integrity, precision, and passion.</p>
-    <a href="" className="button button__flex">
-      say hello
+      <h1 className="home__title">YUVARAJ</h1>
+      <h3 className="home__subtitle">App Developer</h3>
+      <p className="home__description">
+        Building applications with integrity, precision, and passion.
+      </p>
+      <a href="#" className="button button__flex">
+        Say Hello
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="button__icon"
+        >
+          <line x1="22" y1="2" x2="11" y2="13" />
+          <polygon points="22 2 15 22 11 13 2 9 22 2" />
+        </svg>
       </a>
     </div>
   )
