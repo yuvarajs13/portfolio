847863b71c6de596a6d58b0b02b2be66cfbc399c
yuvarajs13 <yuvaraj93600@gmail.com>
2025-09-04 22:48:41 +0530

    bug fixed

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -4,7 +4,7 @@ import nodejsLogo from '../../assets/images/nodejs.png';
 
 const Skills: React.FC = () => {
   const technologies = [
-        { 
+    { 
       name: 'JavaScript', 
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
@@ -26,48 +26,44 @@ const Skills: React.FC = () => {
         </svg>
       )
     },
-{ 
-  name: 'Node.js', 
-  icon: (
-    <img 
-      src={nodejsLogo} 
-      alt="Node.js Logo" 
-      style={{ width: '60px', height: '60px' }} 
-    />
-  )
-},
+    { 
+      name: 'Node.js', 
+      icon: (
+        <img 
+          src={nodejsLogo} 
+          alt="Node.js Logo" 
+          className="skill-image"
+        />
+      )
+    },
     { 
       name: 'Next.js', 
       icon: (
-        <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 180 180"
-    >
-      <mask id="a" maskUnits="userSpaceOnUse" x="0" y="0" width="180" height="180">
-        <circle cx="90" cy="90" r="90" fill="#fff" />
-      </mask>
-      <g mask="url(#a)">
-        <circle cx="90" cy="90" r="90" fill="#000" />
-        <path
-          fill="url(#b)"
-          d="M149.508 157.52L69.335 54H54v71.963h12.195V69.298l72.85 94.222a90.27 90.27 0 0010.463-6z"
-        />
-        <path fill="url(#c)" d="M115 54h12v72h-12z" />
-      </g>
-      <defs>
-        <linearGradient id="b" x1="109" y1="116.5" x2="144.5" y2="160" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#fff" />
-          <stop offset="1" stopColor="#fff" stopOpacity="0" />
-        </linearGradient>
-        <linearGradient id="c" x1="121" y1="54" x2="120.799" y2="106.875" gradientUnits="userSpaceOnUse">
-          <stop stopColor="#fff" />
-          <stop offset="1" stopColor="#fff" stopOpacity="0" />
-        </linearGradient>
-      </defs>
-    </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 180">
+          <mask id="a" maskUnits="userSpaceOnUse" x="0" y="0" width="180" height="180">
+            <circle cx="90" cy="90" r="90" fill="#fff" />
+          </mask>
+          <g mask="url(#a)">
+            <circle cx="90" cy="90" r="90" fill="#000" />
+            <path
+              fill="url(#b)"
+              d="M149.508 157.52L69.335 54H54v71.963h12.195V69.298l72.85 94.222a90.27 90.27 0 0010.463-6z"
+            />
+            <path fill="url(#c)" d="M115 54h12v72h-12z" />
+          </g>
+          <defs>
+            <linearGradient id="b" x1="109" y1="116.5" x2="144.5" y2="160" gradientUnits="userSpaceOnUse">
+              <stop stopColor="#fff" />
+              <stop offset="1" stopColor="#fff" stopOpacity="0" />
+            </linearGradient>
+            <linearGradient id="c" x1="121" y1="54" x2="120.799" y2="106.875" gradientUnits="userSpaceOnUse">
+              <stop stopColor="#fff" />
+              <stop offset="1" stopColor="#fff" stopOpacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
       )
     },
-
   ];
 
   return (
